3fda9f231045375e4bae0617442b1fc93dac8346
“Krystian” <scoverstudio@gmail.com>
2022-08-31 22:34:18 +0200

    rwd + scroll animation

--- src/components/views/Header/Header.js
+++ src/components/views/Header/Header.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import styles from "./Header.module.scss";
+import clsx from "clsx";
 
 const Header = () => {
+  const [scrollHide, setScrollHide] = useState(false);
+
+  const controlScroll = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > 50) {
+        setScrollHide(true);
+      } else setScrollHide(false);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlScroll);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", controlScroll);
+      };
+    }
+  }, []);
+
   return (
     <>
-      <div className={styles.container}>
+      <div className={styles.headerContainer}>
         <div className={styles.box}>
           {/* <h1>Hi,  </h1> */}
           <div className={styles.title}>
@@ -30,14 +53,19 @@ const Header = () => {
             src={`${process.env.PUBLIC_URL}/images/filqImage2.png`}
           ></img>
         </div>
-      </div>
-      <div className={styles.scrollContainer}>
-        <h3>Scroll down </h3>
-        <img
-          alt="filq"
-          src={`${process.env.PUBLIC_URL}/images/mouse-scroll-icon-2.jpg`}
-        />
-        <i className="fa fa-arrow-down" aria-hidden="true"></i>
+        <div
+          className={clsx(
+            styles.scrollContainer,
+            scrollHide && styles.hideScroll
+          )}
+        >
+          <h3>Scroll down </h3>
+          <img
+            alt="filq"
+            src={`${process.env.PUBLIC_URL}/images/mouse-scroll-icon-2.jpg`}
+          />
+          <i className="fa fa-arrow-down" aria-hidden="true"></i>
+        </div>
       </div>
     </>
   );
