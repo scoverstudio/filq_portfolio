8ada11e90e42950e389b471363937d98b0478203
“Krystian” <scoverstudio@gmail.com>
2022-10-11 21:06:15 +0200

    add snipe2die

--- src/components/views/About/About.js
+++ src/components/views/About/About.js
@@ -72,8 +72,13 @@ const About = () => {
             <a href="/portfolio/VALORANT">VALORANTPolska</a>,{" "}
             <a href="/portfolio/Cloud9 CSGO">Cloud9</a>,{" "}
             <a href="/portfolio/NadeKing">NadeKing</a>,{" "}
-            <a href="/portfolio/Snipe2DieTv">Snipe2DieTV</a>, and few more
-            listed{" "}
+            <a
+              href="/portfolio/Snipe2DieTV - CS:GO Channel
+"
+            >
+              Snipe2DieTV
+            </a>{" "}
+            and few more listed{" "}
             <span
               data-text="below"
               onClick={() => scrollIntoClients()}
