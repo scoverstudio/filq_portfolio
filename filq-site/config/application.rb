4922969bafc5dec442021d48eae90a6a80b132f7
“Krystian” <christian.matkowski@gdsoft.info>
2023-01-28 17:15:40 +0100

    application api

--- filq-site/config/application.rb
+++ filq-site/config/application.rb
@@ -12,7 +12,6 @@ require 'action_mailbox/engine'
 require 'action_text/engine'
 require 'action_view/railtie'
 require 'action_cable/engine'
-# require "sprockets/railtie"
 require 'rails/test_unit/railtie'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -27,6 +26,6 @@ module FilqSite
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-        config.api_only = true
+    config.api_only = true
   end
 end
