01e75aa46204bbd27eb43b144f931c9106818746
“Krystian” <christian.matkowski@gdsoft.info>
2023-01-28 18:03:04 +0100

    Revert to 988918ac437fba1c1b7e88211909c7ac1dda640a

--- backend/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  scope :api, defaults: { format: :json } do
-    use_doorkeeper
-    resources :users, only: %i[index show create update destroy]
-    resources :playlists
-    get '/profile', to: 'profile#show'
-    put '/profile', to: 'profile#update'
-  end
-end
