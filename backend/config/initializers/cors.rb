132470404ea4d040a31cc0ad67d4feb1f2b6f9bb
“Krystian” <christian.matkowski@gdsoft.info>
2023-01-28 13:08:00 +0100

    cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'filq.pl', 'filip-szatkowski.netlify.app'
 
     resource '*',
              headers: :any,
