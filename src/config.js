e8808ac32a1b46da7c7ec074691cad414666ce8c
KrystianMatkowski-GDsoft <christian.matkowski@gdsoft.info>
2023-12-15 20:01:19 +0100

    change api

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_URL =
-    process.env.NODE_ENV === "production" ? "https://filq-be-2.onrender.com/api" : "localhost:3001/api";
+    process.env.NODE_ENV === "production" ? "https://filq-be-2.onrender.com/api" : "https://filq-be-2.onrender.com/api";
 
 console.log(API_URL);
