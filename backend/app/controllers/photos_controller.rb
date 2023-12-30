8c4f97585de49eb103d5e8ad1620b7fe2974f1f9
“Krystian” <christian.matkowski@gdsoft.info>
2023-12-30 13:21:11 +0100

    add admin photos

---
+++ backend/app/controllers/photos_controller.rb
@@ -0,0 +1,37 @@
+class PhotosController < ApplicationController
+  before_action :set_photo, only: %i[ show update destroy ]
+
+  def index
+    @photos = Playlist.all
+  end
+
+  def show
+  end
+
+  def create
+    @photo = Photo.create!(photo_params)
+    render :show, status: :created
+  end
+
+  def update
+    if @photo.update(playlist_params)
+      render :show, status: :ok, location: @photo
+    else
+      render json: @photo.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @photo.destroy
+  end
+
+  private
+
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
+
+  def photo_params
+    params.require(:playlist).permit(:name, :link, :height, :width)
+  end
+end
---
+++ backend/db/migrate/20231230114635_create_photos.rb
@@ -0,0 +1,12 @@
+class CreatePhotos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :photos do |t|
+      t.string :name
+      t.integer :width
+      t.integer :height
+      t.string :link
+
+      t.timestamps
+    end
+  end
+end
